1955d7f9d53db8c0983e9a845bb5a483f9abdcc3
sameeranihathe <sameeranihathe@gmail.com>
2018-05-14 23:26:35 +0530

    server error fixed

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var socket = require('socket.io');
 
-//app setup
+// App setup
 var app = express();
 var server = app.listen(4000, function(){
-    console.log("listing to port 4000");
+    console.log('listening for requests on port 4000,');
 });
 
-//static files
+// Static files
 app.use(express.static('public'));
 
-//socket setup
+// Socket setup & pass server
 var io = socket(server);
+io.on('connection', (socket) => {
 
-io.on('connection', function(socket){
-    console.log('socket connection made', socket.id);
+    console.log('made socket connection', socket.id);
 
+    // Handle chat event
     socket.on('chat', function(data){
-        io.sockets.emit('chat'.data);
-    })
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
