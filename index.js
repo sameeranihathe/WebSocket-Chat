f5cb2d083e5bd2452aa83d47781a9f0ec02d64f0
sameeranihathe <sameeranihathe@gmail.com>
2018-05-15 08:53:27 +0530

    innerHTML error fixed

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ io.on('connection', (socket) => {
         io.sockets.emit('chat', data);
     });
 
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
     // Handle typing event
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
--- public/chat.js
+++ public/chat.js
@@ -5,7 +5,8 @@ var socket = io.connect('http://localhost:4000');
 var message = document.getElementById('message'),
       handle = document.getElementById('handle'),
       btn = document.getElementById('send'),
-      output = document.getElementById('output');
+      output = document.getElementById('output'),
+      feedback = document.getElementById('feedback');
 
 // Emit events
 btn.addEventListener('click', function(){
@@ -13,10 +14,18 @@ btn.addEventListener('click', function(){
       message: message.value,
       handle: handle.value
   });
-  message.value = "";
+  
 });
+
+message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
+})
 
 // Listen for events
 socket.on('chat', function(data){
     output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data){
+    feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
 });
